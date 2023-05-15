2d475f6aa6c1622534b434ff8040f4fa16d39baf
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-15 15:42:21 +0200

    Add UserController

---
+++ MyFit-API/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFit_API.Services;
+using MyFit_Libs.Models;
+
+namespace MyFit_API.Controllers
+{
+
+    [ApiController]
+    [Route("/api/User")]
+    public class UserController : ControllerBase
+    {
+        private UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            this._userService = userService;
+        }
+
+        [Route("getUserById")]
+        [HttpGet]
+        public IActionResult GetUserById(long id)
+        {
+            User user = _userService.GetUserById(id);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            return Ok(user);
+        }
+
+
+    }
+}
