18a96686f6470cdb685a9e7ea67bfbf80c237f11
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-15 19:00:24 +0200

    Update User

--- MyFit-Libs/Models/User.cs
+++ MyFit-Libs/Models/User.cs
@@ -13,29 +13,23 @@ public class User {
     public string Mail { get; set;}
     public string State { get; set; }
     public string City { get; set; }
-    public string Street { get; set; }
-    public int CivicNumber { get; set; }
-    public int CAP { get; set; }
     public int IdPlan { get; set;}
     public int IdGym { get; set;}
     public bool IntermittentFasting { get; set;}
 
-    public User(string name, string surname, string mail, string state, string city, string street, int civicNumber, int cAP, int idPlan, int idGym, bool intermittentFasting)
+    public User(string name, string surname, string mail, string state, string city, int idPlan, int idGym, bool intermittentFasting)
     {
         Name = name;
         Surname = surname;
         Mail = mail;
         State = state;
         City = city;
-        Street = street;
-        CivicNumber = civicNumber;
-        CAP = cAP;
         IdPlan = idPlan;
         IdGym = idGym;
         IntermittentFasting = intermittentFasting;
     }
 
-    public User(long id, string name, string surname, string mail, string state, string city, string street, int civicNumber, int cAP, int idPlan, int idGym, bool intermittentFasting)
+    public User(long id, string name, string surname, string mail, string state, string city, int idPlan, int idGym, bool intermittentFasting)
     {
         Id = id;
         Name = name;
@@ -43,9 +37,6 @@ public class User {
         Mail = mail;
         State = state;
         City = city;
-        Street = street;
-        CivicNumber = civicNumber;
-        CAP = cAP;
         IdPlan = idPlan;
         IdGym = idGym;
         IntermittentFasting = intermittentFasting;
