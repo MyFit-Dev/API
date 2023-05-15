9bdb1e90ebceb922280ba1795c69dcdb2d9695fa
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-15 15:42:49 +0200

    Add UserService

---
+++ MyFit-API/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using MyFit_API.Repositories;
+using MyFit_Libs.Models;
+
+namespace MyFit_API.Services
+{
+    public class UserService
+    {
+        private UserRepository _userRepository = new UserRepository();
+
+        public User? GetUserById(long id)
+        {
+            return _userRepository.GetUserById(id);
+        }
+
+    }
+}
