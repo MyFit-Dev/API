1d39e159f7d09ecec4b6e5dc06dc9b4da030c3e0
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-15 15:42:37 +0200

    Add UserRepository

---
+++ MyFit-API/Repositories/UserRepository.cs
@@ -0,0 +1,20 @@
+﻿using MyFit_API.Database;
+using MyFit_Libs.Models;
+using System.Data.SqlClient;
+
+namespace MyFit_API.Repositories
+{
+    public class UserRepository
+    {
+
+        internal User? GetUserById(long id)
+        {
+            string query = "SELECT * FROM Users WHERE id = @_id";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("@_id", id);
+            return DatabaseManager<User>.GetInstance().MakeQueryOneResult(cmd);
+        }
+
+
+    }
+}
