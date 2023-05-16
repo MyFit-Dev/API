f7eec420e808fd5a2897997fd590f2d8a8b43f47
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-16 21:50:14 +0200

    Aggiunte eccezioni su Plan

---
+++ MyFit-API/Exceptions/PlanException/PlanAlredyFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.PlanException
+{
+    [Serializable]
+    public class PlanAlredyFoundException : Exception
+    {
+        public PlanAlredyFoundException()
+        {
+        }
+
+        public PlanAlredyFoundException(string? message) : base(message)
+        {
+        }
+
+        public PlanAlredyFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected PlanAlredyFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+    }
+}
---
+++ MyFit-API/Exceptions/PlanException/PlanNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.PlanException
+{
+    [Serializable]
+    public class PlanNotFoundException : Exception
+    {
+        public PlanNotFoundException()
+        {
+        }
+
+        public PlanNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public PlanNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected PlanNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
