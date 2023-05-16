1f8cb307924f2fa5088f4f4ca80f5386aa9ca64f
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-16 21:50:00 +0200

    Aggiunte eccezioni su Gym

---
+++ MyFit-API/Exceptions/GymException/GymAlredyFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.GymException
+{
+    [Serializable]
+    public class GymAlredyFoundException : Exception
+    {
+        public GymAlredyFoundException()
+        {
+        }
+
+        public GymAlredyFoundException(string? message) : base(message)
+        {
+        }
+
+        public GymAlredyFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected GymAlredyFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ MyFit-API/Exceptions/GymException/GymNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.GymException
+{
+    [Serializable]
+    public class GymNotFoundException : Exception
+    {
+        public GymNotFoundException()
+        {
+        }
+
+        public GymNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public GymNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected GymNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
