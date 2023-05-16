8f2c93c9a27a8fa0580d27fdc6fb6963286284d4
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-16 16:35:24 +0200

    Aggiunte query SQL per utente utili ad ottenere Piano, Palestra, Digiuno_Intermittente e altro

--- MyFit-API/Repositories/UserRepository.cs
+++ MyFit-API/Repositories/UserRepository.cs
@@ -9,10 +9,42 @@ namespace MyFit_API.Repositories
 
         internal User? GetUserById(long id)
         {
-            string query = "SELECT * FROM Utenti WHERE Id = @_id";
+            string query = "SELECT * FROM [User] WHERE Id = @_id";
             SqlCommand cmd = new SqlCommand(query);
             cmd.Parameters.AddWithValue("@_id", id);
             return DatabaseManager<User>.GetInstance().MakeQueryOneResult(cmd);
+        }
+
+        internal User? GetUserByEmail(string mail)
+        {
+            string query = "SELECT * FROM [User] WHERE Mail = '@_mail'";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("@_mail", mail);
+            return DatabaseManager<User>.GetInstance().MakeQueryOneResult(cmd);
+        }
+
+        internal Plan GetUserPlan(long id)
+        {
+            string query = "SELECT p.Id, p.Name, p.Value, p.Price, p.Description FROM [User] u JOIN [Plan] p ON p.Id = u.IdPlan WHERE u.Id = @_id";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("@_id", id);
+            return DatabaseManager<Plan>.GetInstance().MakeQueryOneResult(cmd);
+        }
+
+        internal Gym? GetUserGym(long id)
+        {
+            string query = "SELECT g.Id, g.Name, g.IdStaff, g.State, g.City, g.Street, g.CivicNumber, g.CAP FROM [User] u JOIN [Gym] ON g.Id = u.IdGym WHERE u.Id = @_id";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("@_id", id);
+            return DatabaseManager<Gym>.GetInstance().MakeQueryOneResult(cmd);
+        }
+
+        internal bool IsUserOnIntermittentFasting(long id)
+        {
+            string query = "SELECT IntermittentFasting FROM [User] WHERE Id = @_id";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("@_id", id);
+            return DatabaseManager<bool>.GetInstance().MakeQueryOneResult(cmd);
         }
 
 
