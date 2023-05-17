28c57d6d26dab2644965ddf7d23b9718193e59b6
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-17 20:44:44 +0200

    Create eccezione DietNotFoundException

---
+++ MyFit-API/Exceptions/DietException/DietNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.DietException
+{
+    public class DietNotFoundException : Exception
+    {
+        public DietNotFoundException()
+        {
+        }
+
+        public DietNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public DietNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected DietNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
