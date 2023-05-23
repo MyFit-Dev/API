e2ecb833588271ae5ba626fd30eb1fe2f9ea422c
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-23 16:11:25 +0200

    Creato FormNotFoundException

---
+++ MyFit-API/Exceptions/FormException/FormNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.FormException
+{
+    public class FormNotFoundException : Exception
+    {
+        public FormNotFoundException()
+        {
+        }
+
+        public FormNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public FormNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected FormNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
