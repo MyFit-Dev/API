9aede35fd6e980aceacb8463b12212e5a49b044a
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-24 01:12:32 +0200

    Creata eccezione per gestire gli Esercizi

---
+++ MyFit-API/Exceptions/ExerciseException/ExerciseException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.ExerciseException
+{
+    public class ExerciseException : Exception
+    {
+        public ExerciseException()
+        {
+        }
+
+        public ExerciseException(string? message) : base(message)
+        {
+        }
+
+        public ExerciseException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ExerciseException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
