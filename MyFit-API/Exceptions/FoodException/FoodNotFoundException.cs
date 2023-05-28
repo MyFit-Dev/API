5ef0791abe8f4d57c6a8307ee78f31384db158b9
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-29 00:32:01 +0200

    Creata FoodNotFoundException

---
+++ MyFit-API/Exceptions/FoodException/FoodNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.FoodException
+{
+    public class FoodNotFoundException : Exception
+    {
+        public FoodNotFoundException()
+        {
+        }
+
+        public FoodNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public FoodNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected FoodNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
