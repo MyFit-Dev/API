b11cd57adb43419e45aac33c59eac7f045a0080e
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-29 00:54:31 +0200

    Creato FoodService

---
+++ MyFit-API/Services/FoodService.cs
@@ -0,0 +1,60 @@
+﻿using MyFit_API.Exceptions.FoodException;
+using MyFit_API.Repositories;
+using MyFit_Libs.Models;
+using System.Xml.Linq;
+
+namespace MyFit_API.Services
+{
+    public class FoodService
+    { 
+        private FoodRepository _foodRepository = new FoodRepository();
+
+        public List<Food> GetAllFoods()
+        {
+            List<Food>? foods = _foodRepository.GetAllFoods();
+
+            return foods != null ? foods : throw new FoodNotFoundException("Foods not found");
+        }
+
+        public List<Food> GetSimilarFoods(string name, int results)
+        {
+            List<Food>? foods = _foodRepository.GetSimilarFoods(name, results);
+
+            return foods != null ? foods : throw new FoodNotFoundException("Foods not found");
+        }
+
+        public long CountFoods()
+        {
+            return _foodRepository.CountFoods();
+        }
+
+        public Food? GetFood(long id)
+        {
+            Food? food = _foodRepository.GetFood(id);
+
+            return food != null ? food : throw new FoodNotFoundException("Food not found");
+        }
+
+        public void AddFood(Food food)
+        {
+            _foodRepository.AddFood(food);
+        }
+
+        public void DeleteFood(long id) 
+        {
+            if (!_foodRepository.ExistsFood(id))
+                throw new FoodNotFoundException("Food not found");
+
+            _foodRepository.DeleteFood(id);
+        }
+
+        public void UpdateFood(Food food, long id)
+        {
+            if (!_foodRepository.ExistsFood(id))
+                throw new FoodNotFoundException("Food not found");
+
+            _foodRepository.DeleteFood(id);
+            _foodRepository.AddFood(food);
+        }
+    }
+}
