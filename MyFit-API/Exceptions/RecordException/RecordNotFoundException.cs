34216ee56ce5bd9ab46943efefed28103ddb32ce
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-29 16:15:03 +0200

    Creato RecordException

---
+++ MyFit-API/Exceptions/RecordException/RecordNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.RecordException
+{
+    public class RecordNotFoundException : Exception
+    {
+        public RecordNotFoundException()
+        {
+        }
+
+        public RecordNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public RecordNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected RecordNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
