863de493bac500cd3e709390d65161168ca79d23
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-31 23:46:33 +0200

    Creato LogNotFoundException

---
+++ MyFit-API/Exceptions/LogException/LogNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.LogException
+{
+    public class LogNotFoundException : Exception
+    {
+        public LogNotFoundException()
+        {
+        }
+
+        public LogNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public LogNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected LogNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
