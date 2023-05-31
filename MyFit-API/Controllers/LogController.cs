a9e8b392f52241f2e8afa0e38dc45774c2e9784c
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-05-31 23:46:51 +0200

    Creato LogController e LogService

---
+++ MyFit-API/Controllers/LogController.cs
@@ -0,0 +1,257 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using MyFit_API.Exceptions.LogException;
+using MyFit_API.Services;
+using MyFit_Libs.Models;
+
+namespace MyFit_API.Controllers
+{
+
+    [EnableCors]
+    [ApiController]
+    [Route("/api/Log")]
+    public class LogController : ControllerBase
+    {
+
+        private LogService _logService;
+
+        public LogController(LogService logService)
+        {
+            _logService = logService;
+        }
+
+        [Route("getAll")]
+        [HttpGet]
+        public IActionResult GetAllLogs()
+        {
+            try
+            {
+                return Ok(_logService.GetAllLogs());
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("get")]
+        [HttpGet]
+        public IActionResult GetLog(long id)
+        {
+            try
+            {
+                return Ok(_logService.GetLog(id));
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("getText")]
+        [HttpGet]
+        public IActionResult GetLogText(long id)
+        {
+            try
+            {
+                return Ok(_logService.GetLogText(id));
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("getScope")]
+        [HttpGet]
+        public IActionResult GetLogScope(long id)
+        {
+            try
+            {
+                return Ok(_logService.GetLogScope(id));
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("getDate")]
+        [HttpGet]
+        public IActionResult GetLogDate(long id)
+        {
+            try
+            {
+                return Ok(_logService.GetLogDate(id));
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("getIdUser")]
+        [HttpGet]
+        public IActionResult GetLogIdUser(long id)
+        {
+            try
+            {
+                return Ok(_logService.GetLogIdUser(id));
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("getValue")]
+        [HttpGet]
+        public IActionResult GetLogValue(long id)
+        {
+            try
+            {
+                return Ok(_logService.GetLogValue(id));
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("add")]
+        [HttpPost]
+        public IActionResult AddLog(Log log)
+        {
+            if (log == null)
+                return BadRequest("Log is null");
+
+            _logService.AddLog(log);
+            return Ok();
+        }
+
+        [Route("setText")]
+        [HttpPut]
+        public IActionResult SetLogText(long id, string text)
+        {
+            if (text == null)
+                return BadRequest("Text is null");
+
+            try
+            {
+                _logService.SetLogText(id, text);
+                return Ok();
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("setScope")]
+        [HttpPut]
+        public IActionResult SetLogScope(long id, string scope)
+        {
+            if (scope == null)
+                return BadRequest("Scope is null");
+
+            try
+            {
+                _logService.SetLogScope(id, scope);
+                return Ok();
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("setIdUser")]
+        [HttpPut]
+        public IActionResult SetLogIdUser(long id, long? idUser)
+        {
+            if (idUser == null)
+                return BadRequest("IdUser is null");
+
+            try
+            {
+                _logService.SetLogIdUser(id, idUser);
+                return Ok();
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("setValue")]
+        [HttpPut]
+        public IActionResult SetLogValue(long id, byte? value)
+        {
+            if (value == null)
+                return BadRequest("Value is null"); 
+
+            try
+            {
+                _logService.SetLogValue(id, value);
+                return Ok();
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("delete")]
+        [HttpDelete]
+        public IActionResult DeleteLog(long id)
+        {
+            try
+            {
+                _logService.DeleteLog(id);
+                return Ok();
+            }
+            catch (LogNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Route("deleteUserLogs")]
+        [HttpDelete]
+        public IActionResult DeleteUserLogs(long idUser)
+        {
+            _logService.DeleteUserLogs(idUser);
+            return Ok();
+        }
+
+        [Route("deleteBefore")]
+        [HttpDelete]
+        public IActionResult DeleteLogsBeforeDate(DateTime date)
+        {
+            _logService.DeleteLogsBeforeDate(date);
+            return Ok();
+        }
+
+        [Route("deleteBetween")]
+        [HttpDelete]
+        public IActionResult DeleteLogsBetweenDates(DateTime date1, DateTime date2)
+        {
+            _logService.DeleteLogsBetweenDates(date1, date2);
+            return Ok();
+        }
+
+        [Route("deleteByValue")]
+        [HttpDelete]
+        public IActionResult DeleteLogsByValue(byte value)
+        {
+            _logService.DeleteLogsByValue(value);
+            return Ok();
+        }
+
+
+
+        
+
+
+    }
+}
---
+++ MyFit-API/Services/LogService.cs
@@ -0,0 +1,118 @@
+﻿using MyFit_API.Exceptions.LogException;
+using MyFit_API.Repositories;
+using MyFit_Libs.Models;
+
+namespace MyFit_API.Services
+{
+    public class LogService
+    {
+
+        private LogRepository _logRepository = new LogRepository();
+
+        public List<Log> GetAllLogs()
+        {
+            List<Log>? logs = _logRepository.GetAllLogs();
+
+            return logs != null ? logs : throw new LogNotFoundException("Logs not found");
+        }
+
+        public Log GetLog(long id)
+        {
+            Log? log = _logRepository.GetLog(id);
+
+            return log != null ? log : throw new LogNotFoundException("Log not found");
+        }
+
+        public string GetLogText(long id)
+        {
+            string? text = _logRepository.GetLogText(id);
+
+            return text != null ? text : throw new LogNotFoundException("Log not found");
+        }
+
+        public string GetLogScope(long id)
+        {
+            string? scope = _logRepository.GetLogScope(id);
+
+            return scope != null ? scope : throw new LogNotFoundException("Log not found");
+        }
+
+        public DateTime? GetLogDate(long id)
+        {
+            DateTime? date = _logRepository.GetLogDate(id);
+
+            return date != null ? date : throw new LogNotFoundException("Log not found");
+        }   
+
+        public long? GetLogIdUser(long id)
+        {
+            long? idUser = _logRepository.GetLogUserId(id);
+
+            return idUser != null ? idUser : throw new LogNotFoundException("Log not found");
+        }
+
+        public byte? GetLogValue(long id)
+        {
+            byte? value = _logRepository.GetLogValue(id);
+
+            return value != null ? value : throw new LogNotFoundException("Log not found");
+        }
+
+        public void AddLog(Log log)
+        {
+            _logRepository.AddLog(log);
+        }   
+
+        public void SetLogText(long id, string text)
+        {
+            _logRepository.SetLogText(id, text);
+        }
+
+        public void SetLogScope(long id, string scope)
+        {
+            _logRepository.SetLogScope(id, scope);
+        }
+
+        public void SetLogIdUser(long id, long? idUser)
+        {
+            _logRepository.SetLogIdUser(id, idUser);
+        }
+
+        public void SetLogValue(long id, byte? value)
+        {
+            _logRepository.SetLogValue(id, value);
+        }
+
+        public void DeleteLog(long id)
+        {
+            if (!_logRepository.ExistsLog(id))
+                throw new LogNotFoundException("Log not found");
+
+            _logRepository.DeleteLog(id);
+        }
+
+        public void DeleteUserLogs(long idUser)
+        {
+            _logRepository.DeleteUserLogs(idUser);
+        }
+
+        public void DeleteLogsBeforeDate(DateTime date)
+        {
+            _logRepository.DeleteLogsBefore(date);
+        }
+
+        public void DeleteLogsBetweenDates(DateTime date1, DateTime date2)
+        {
+            _logRepository.DeleteLogsBetweenDates(date1, date2);
+        }
+
+        public void DeleteLogsByValue(byte value)
+        {
+            _logRepository.DeleteLogsByValue(value);
+        }
+
+
+
+
+    }
+}
