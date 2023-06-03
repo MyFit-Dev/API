69a4d9f5218480854645078a81d32c13842a500f
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-06-03 23:47:00 +0200

    Creato DataRecordRepository

---
+++ MyFit-API/Repositories/DataRecordRepository.cs
@@ -0,0 +1,19 @@
+﻿using MyFit_API.Database;
+using MyFit_Libs.Models;
+using System.Data.SqlClient;
+
+namespace MyFit_API.Repositories
+{
+    public class DataRecordRepository
+    {
+
+        internal List<DataRecord>? GetAll()
+        {
+            string query = "SELECT * FROM DataRecord";
+            SqlCommand cmd = new SqlCommand(query);
+
+            return DatabaseManager<List<DataRecord>?>.GetInstance().MakeQueryMoreResults(cmd);
+        }
+
+    }
+}
