7a8eaf4a62ee764de1e3c94971053f9e1f696625
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-06-05 01:37:02 +0200

    Creato Permission exception

---
+++ MyFit-API/Exceptions/PermissionException/PermissionNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.PermissionException
+{
+    public class PermissionNotFoundException : Exception
+    {
+        public PermissionNotFoundException()
+        {
+        }
+
+        public PermissionNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public PermissionNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected PermissionNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
