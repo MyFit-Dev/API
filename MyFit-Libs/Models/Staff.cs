a7b1a0a36a1fc81f1f7644061c78578ed527b952
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-06-05 01:38:32 +0200

    Modificato Staff.Cs

--- MyFit-Libs/Models/Staff.cs
+++ MyFit-Libs/Models/Staff.cs
@@ -7,23 +7,27 @@ namespace MyFit_Libs.Models
 {
     public class Staff
     {
-        public string Id { get; set; }
-        public int IdUser { get; set; }
-        public int IdType { get; set; }
+        public int Id { get; set; }
+        public long IdUser { get; set; }
+        public byte IdType { get; set; }
+
+        public long IdGym { get; set; }
 
         public Staff() {}
 
-        public Staff(string id, int idUser, int idType)
+        public Staff(int id, long idUser, byte idType, long idGym)
         {
             Id = id;
             IdUser = idUser;
             IdType = idType;
+            IdGym = idGym;
         }
 
-        public Staff(int idUser, int idType)
+        public Staff(long idUser, byte idType, long idGym)
         {
             IdUser = idUser;
             IdType = idType;
+            IdGym = idGym;
         }
     }
 }
