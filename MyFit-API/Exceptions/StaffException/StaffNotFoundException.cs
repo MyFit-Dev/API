ba958f72b5de89ce2bfe63b2d9eb1abfbd5a36b2
AEndrix <38986519+AEndrix03@users.noreply.github.com>
2023-06-05 01:37:56 +0200

    Creato Staff Exception

---
+++ MyFit-API/Exceptions/StaffException/StaffNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace MyFit_API.Exceptions.StaffException
+{
+    public class StaffNotFoundException : Exception
+    {
+        public StaffNotFoundException()
+        {
+        }
+
+        public StaffNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public StaffNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected StaffNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
